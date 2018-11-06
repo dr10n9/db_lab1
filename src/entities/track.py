2ad52c8963e98d96ea1513a43ef677f9680801be
dr10n9 <dr10n9@gmail.com>
2018-11-06 14:33:43 +0000

    upd

--- src/entities/track.py
+++ src/entities/track.py
@@ -1,12 +1,10 @@
 class Track:
-    def __init__(self, name, length, number, id=0):
+    def __init__(self, name, length, id=0):
         self.name = name
         self.length = length
-        self.number = number
         self.id = id
 
     def print_self(self):
         print("id: ", self.id)
         print("name: ", self.name)
         print("length: ", self.length)
-        print("number: ", self.number)
